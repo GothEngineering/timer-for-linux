eaa52a494a902a0321a6db457e957464fcbc25ff
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-07-10 10:05:11 -0400

    Initial commit

--- timersforlinux.py
+++ timersforlinux.py
@@ -1,1 +1,19 @@
+import customtkinter
+import tkinter
+import time
 
+root = customtkinter.CTk()
+root.title("Timer")
+root.geometry("400x300")
+
+
+label_test = customtkinter.CTkLabel(root, text="Test insano", text_color="BLACK")
+label_test.pack()
+
+canvas = customtkinter.CTkCanvas(root, width=200, height=200)
+canvas.pack()
+
+canvas.create_aa_circle(100, 100, 60)
+
+
+root.mainloop()
