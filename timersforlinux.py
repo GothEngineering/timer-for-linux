3dd248fc331995d0903b7ddb8a0df6ae4a4c35f4
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-07-14 20:58:20 -0400

    Add a class for the timer, alongside the prototype of the timer arc

--- timersforlinux.py
+++ timersforlinux.py
@@ -6,14 +6,23 @@ root = customtkinter.CTk()
 root.title("Timer")
 root.geometry("400x300")
 
-
-label_test = customtkinter.CTkLabel(root, text="Test insano", text_color="BLACK")
-label_test.pack()
-
-canvas = customtkinter.CTkCanvas(root, width=200, height=200)
-canvas.pack()
-
-canvas.create_aa_circle(100, 100, 60)
+class Timer:
+    def __init__(self):
+        self.label_test = customtkinter.CTkLabel(root, text=f"Timer", text_color="white", fg_color="black")
+        self.label_test.pack(fill="both")
+        
 
 
+        self.timer_circle = customtkinter.CTkCanvas(root, height=400, width=300, bg="gray")
+        self.timer_circle.pack(expand=True, fill="both")
+                                                      #X1, Y1, X2, Y2. That's the coordinates of each number
+        self.pie_chart = self.timer_circle.create_arc(100, 40, 300, 240, start=90, extent=359.9, width=10, outline="red", style="arc")
+        # Remember, X1 and Y1 are the starting point of the rectangle, X2 and Y2 are the finish point. Because I wanted the arc to be centered
+        # I substracted the first and the latter Y's by half the amount of the width and height (supposedly but it works now atleast)
+        
+
+   
+
+
+timer = Timer()
 root.mainloop()
