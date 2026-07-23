8c498f90aa8782bc6d489953a536bbb65538e519
FosterLundwick <279334107+GothEngineering@users.noreply.github.com>
2026-07-23 10:28:20 -0400

    Add timer animation with the arc alongside a label

--- timersforlinux.py
+++ timersforlinux.py
@@ -4,44 +4,53 @@ import time
 
 root = customtkinter.CTk()
 root.title("Timer")
-root.geometry("420x360")
+# Reminder: X and Y
+root.geometry("420x415") # Im sorry for changing the resolution to fit the tkinter buttons :sob:
 root.config(bg="gray20") # Find a more pretty colour please, this gray looks a little too intense
 # Remember you have to change the background of each label and button to the same colour so it doesn't clash with the real bg
 
 class Timer:
 
     def __init__(self):
+        
         self.counting_down = False
         self.hours_placeholder = "00"
         self.minutes_placeholder = "00"
         self.seconds_placeholder = "00"  
+        self.current_seconds = 0
+        
+        # TO DO: make this label change depending on the time inputted, god bless me
+        self.label_text = customtkinter.CTkLabel(root, text="00:00:00", text_color="white", bg_color="gray20")
+        self.label_text.grid(row=1, column=1, columnspan=3)
 
-        
-        self.button_test = customtkinter.CTkButton(root, text="Start", text_color="white", fg_color="black", command=self.countdown_start, 
+        self.start_button = customtkinter.CTkButton(root, text="Start", text_color="white", fg_color="black", command=self.countdown_start, 
         bg_color="gray20")
-        self.button_test.grid(row=3, column=2)
+        self.start_button.grid(row=4, column=2)
         
+        self.pause_button = customtkinter.CTkButton(root, text="Pause", text_color="white", fg_color="black", bg_color="gray20",
+        command=self.pause_timer)
+        self.pause_button.grid(row=5, column=2)
 
         self.hours_input = customtkinter.CTkEntry(root, font=("calibre", 15, "normal"), bg_color="gray20",
         placeholder_text="Hours", placeholder_text_color="gray50", justify="center")
-        self.hours_input.grid(row=1, column=1)
+        self.hours_input.grid(row=2, column=1)
         
         
         self.minutes_input = customtkinter.CTkEntry(root, font=("calibre", 15, "normal"), bg_color="gray20",
         placeholder_text="Minutes", placeholder_text_color="gray50", justify="center")
-        self.minutes_input.grid(row=1, column=2)
+        self.minutes_input.grid(row=2, column=2)
         
         
         self.seconds_input = customtkinter.CTkEntry(root, font=("calibre", 15, "normal"), bg_color="gray20",
         placeholder_text="Seconds", placeholder_text_color="gray50", justify="center")
-        self.seconds_input.grid(row=1, column=3)
+        self.seconds_input.grid(row=2, column=3)
         
         
         self.timer_circle = customtkinter.CTkCanvas(root, height=300, width=300, bg="gray30")
         
                                                       #X1, Y1, X2, Y2. That's the coordinates of each number
-        self.pie_chart = self.timer_circle.create_arc(50, 30, 250, 230, start=90, extent=359.9, width=10, outline="red", style="arc")
-        self.timer_circle.grid(row=2, column=1, columnspan=3)
+        self.pie_chart = self.timer_circle.create_arc(50, 50, 250, 250, start=90, extent=359.9, width=10, outline="red", style="arc")
+        self.timer_circle.grid(row=3, column=1, columnspan=3)
         # Remember, X1 and Y1 are the starting point of the rectangle, X2 and Y2 are the finish point. Because I wanted the arc to be centered
         # I substracted the first and the latter Y's by half the amount of the width and height (supposedly but it works now atleast)
     
@@ -50,7 +59,7 @@ class Timer:
     
     def countdown_start(self):
         self.start_time = time.time()
-        self.counting_down = not self.counting_down
+        #self.counting_down = not self.counting_down
         print(f"La variable al activar es: {self.counting_down}")
         
         
@@ -89,36 +98,40 @@ class Timer:
         seconds = self.seconds_entry
 
         self.total_seconds = (int(hours) * 3600) + (int(minutes) * 60) + int(seconds)
-    
+        self.current_seconds = self.total_seconds
+
         self.counting_timer()
 
     
-    
     def counting_timer(self):
         
-        #hours = self.hours_entry
-        #minutes = self.minutes_entry
-        #seconds = self.seconds_entry
+        # This "counting down" variable is causing issues, make sure to turn it to true whenever i press start
+        # or remove it alltogether LMAO
+        # it seems the issue is that pressing the start button changes it to true, but if something happens and it becomes
+        # false, it needs to be changed AGAIN with the pause button to start counting down again
+        if self.current_seconds > 0 and self.counting_down == True:
+            self.current_seconds -= 1
+            degrees = (self.current_seconds / self.total_seconds) * 360
+            self.timer_circle.itemconfig(self.pie_chart, extent=degrees)
+            print(f"Ticking down... {self.current_seconds}")
+            
 
-        #self.total_seconds = (int(hours) * 3600) + (int(minutes) * 60) + int(seconds)
-        #print(f"tiempo total papu: {self.total_seconds}")
-        
-        if self.total_seconds > 0:
-            self.total_seconds -= 1
-            print(f"Ticking down... {self.total_seconds}")
-            
-            if self.total_seconds == 0:
+            if self.current_seconds == 0:
+                self.current_seconds = 0
                 print("YIPPIEEE")
-            
+                self.timer_circle.itemconfig(self.pie_chart, extent=359.9)
             root.after(1000, self.counting_timer)
 
+    def pause_timer(self):
+        if self.counting_down:
+            self.pause_button.configure(text="metelo papi metelo")
+        else:
+            self.pause_button.configure(text="Pause")
+        self.counting_down = not self.counting_down
+        print(self.counting_down)
         
-
         
         
         
-
-
-
 timer = Timer()
 root.mainloop()
